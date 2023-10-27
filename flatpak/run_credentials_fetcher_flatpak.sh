fac844e1749f820757556542b5b736e92fbc061f
root <root@ip-10-0-56-18.example.com>
2023-10-27 06:10:48 +0000

    still working, WIP

--- flatpak/run_credentials_fetcher_flatpak.sh
+++ flatpak/run_credentials_fetcher_flatpak.sh
@@ -12,6 +12,7 @@ mkdir -p /var/credentials-fetcher/logging/
 export PATH=$PATH:/app/lib/sdk/dotnet6/bin
 export DOTNET_ROOT=/app/lib/sdk/dotnet6/lib
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+export SASL_PATH=/app/lib/sasl2
 #LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/lib/sdk/dotnet6/lib
 
 ./credentials-fetcherd -s gmsa-plugin-input
